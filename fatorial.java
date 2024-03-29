5d01d351f5415adef8795dbb7bc9ed951ce04731
Rafael William Dresch <117855490+RafinhaWD@users.noreply.github.com>
2024-03-29 19:38:22 -0300

    Update and rename .gitignore to fatorial.java

---
+++ fatorial.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número para calcular o fatorial: ");
+        int numero = scanner.nextInt();
+       
+        long fatorial = 1;
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+        
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        scanner.close();
+    }
+}
